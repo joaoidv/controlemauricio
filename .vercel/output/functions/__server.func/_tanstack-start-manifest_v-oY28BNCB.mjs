82164039e13db644837bf232095fa90359acb7f8
João Guilherme <joaoguilherme.idv@gmail.com>
2026-09-09 07:10:45 -0400

    build: generate Vercel output and SSR deployment artifacts

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-oY28BNCB.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-oY28BNCB.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "C:/Users/João Guilherme/Documents/Viccon/Maurício/controlemauricio/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-jMujHugF.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-jMujHugF.js"
+		} }]
+	},
+	"/": {
+		filePath: "C:/Users/João Guilherme/Documents/Viccon/Maurício/controlemauricio/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DDg4tOg9.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
